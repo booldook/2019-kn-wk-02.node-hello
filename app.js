94730db78544e8b5b9dd18346dfe48a9e55993a5
hee20192 <html20192@daum.net>
2019-10-20 14:30:56 +0900

    app.js 연습

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const app = express();
+
+app.listen(3000, function(){
+  console.log("http://127.0.0.1:3000");
+});
+
+app.get("/hello", function (req, res) {
+  var name = req.query.name;
+  res.send(`<h1>${name}님 반갑습니다.</h1>`);
+})
